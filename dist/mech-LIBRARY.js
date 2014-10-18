22a2ef05dfd208aff3abd5aa5915670f31f7dc40
Eric Hosick <erichosick@gmail.com>
2014-10-17 18:24:10 -0700

    * Version out of sync

--- dist/mech-LIBRARY.js
+++ dist/mech-LIBRARY.js
@@ -1,5 +1,5 @@
 // mech-library.js
-// version: 0.1.4
+// version: 0.1.5
 // author: YOUR INFORMATION
 // license: MIT
 (function() {
@@ -9,7 +9,7 @@ var root = this; // window (browser) or exports (server)
 var m = root.m || { library : { _ : {} } }; // merge with previous or new module
 m.library = m.library || { library : { _ : {} } }; // merge with pervious or new sub-module
 m.library._ = m.library._ || { _ : {}}; // merge with pervious or new sub-module
-m.library._["version-library"] = '0.1.4'; // version set through gulp build
+m.library._["version-library"] = '0.1.5'; // version set through gulp build
 
 // export module for node or the browser
 if(typeof module !== 'undefined' && module.exports) {
--- tests/check-build.js
+++ tests/check-build.js
@@ -7,6 +7,6 @@ describe("the modules were built correctly", function() {
    });
    
    it ("should have the correct version", function() {
-      expect(m.library._["version-" + pkg.namesub]).to.equal('0.1.4');
+      expect(m.library._["version-" + pkg.namesub]).to.equal('0.1.5');
    });
 });
